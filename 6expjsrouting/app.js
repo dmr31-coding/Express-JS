395e75a45b31fbc0d3e6f9b41eee75ec4767eb87
Dinesh Magar <magard1010@gmail.com>
2024-11-26 23:46:41 +0545

    an array of callback

--- 6expjsrouting/app.js
+++ 6expjsrouting/app.js
@@ -23,14 +23,30 @@ const port = process.env.PORT || '3000'
 // })
 
 //More than one callback 
-app.get('/callbackegg', (req, res, next) => {
-    console.log('First callback')
-    next()
+// app.get('/callbackegg', (req, res, next) => {
+//     console.log('First callback')
+//     next()
     
-}, (req, res) => {
-    console.log('Second callback')
-    res.send('More than One callback example')
-})
+// }, (req, res) => {
+//     console.log('Second callback')
+//     res.send('More than One callback example')
+// })
+
+// an array of callback
+const cb1 = (req, res, next) => {
+    console.log('first callback')
+    next()
+}
+const cb2 = (req, res, next) => {
+    console.log('second callback')
+    next()
+}
+const cb3 = (req, res) => {
+    console.log('third callback')
+    res.send('an array of callback example')
+}
+app.get('/callbeg', [cb1, cb2, cb3])
+
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
