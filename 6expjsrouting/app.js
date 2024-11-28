1975cfb02574d711641445d0af0907d89525fe16
Dinesh Magar <magard1010@gmail.com>
2024-11-28 21:29:39 +0545

    chained route callback- .route method

--- 6expjsrouting/app.js
+++ 6expjsrouting/app.js
@@ -49,24 +49,53 @@ const port = process.env.PORT || '3000'
 
 
 // combination of independent fn. and array of fn.
-const cb1 = (req, res, next) => {
-    console.log('first callback')
-    next()
-}
-const cb2 = (req, res, next) => {
-    console.log('second callback')
-    next()
-}
-app.get('/callbegone', [cb1, cb2], (req, res, next) => {
-    console.log('third callback')
-    next()
-}, (req, res) => {
-    console.log('fourth callback')
-    res.send('combination of independent fn. and array of fn.')
-}
+// const cb1 = (req, res, next) => {
+//     console.log('first callback')
+//     next()
+// }
+// const cb2 = (req, res, next) => {
+//     console.log('second callback')
+//     next()
+// }
+// app.get('/callbegone', [cb1, cb2], (req, res, next) => {
+//     console.log('third callback')
+//     next()
+// }, (req, res) => {
+//     console.log('fourth callback')
+//     res.send('combination of independent fn. and array of fn.')
+// }
 
-)
+// )
 
+// chained route callback
+// app.route('/student')
+// .get((req, res) => {
+//     res.send('All student')
+// })
+// .post((req, res) => {
+//     res.send('add new student')
+// })
+// .put((req, res) => {
+//     res.send('update student')
+// })
+
+app.route('/student')
+.all((req, res, next) => {
+    console.log('First run this for all http method')
+    next()
+})
+.get((req, res) => {
+    console.log('GET method')
+    res.send('All student')
+})
+.post((req, res) => {
+    console.log('POST method')
+    res.send('add new student')
+})
+.put((req, res) => {
+    console.log('PUT method')
+    res.send('update student')
+})
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
