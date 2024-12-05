00c1b88b0ab8ffb73f2d6d2865c65fcab43d3279
Dinesh Magar <magard1010@gmail.com>
2024-12-05 23:59:38 +0545

    controller need explanation

--- controllerinexp/app.js
+++ controllerinexp/app.js
@@ -6,6 +6,22 @@ const app = express()
 
 const port = process.env.PORT || '3000'
 
+
+app.get('/student/all', (req, res) => {
+    res.send('All student')
+})
+
+app.get('/student/delete/:id([0-9]{2})', (req, res) => {
+    console.log(req.params)
+    const {id} = req.params
+    console.log(id)
+    if (id == 10) {
+        res.send('this id cant be deleted')
+    } else {
+        res.send(`student delete ${id}`)
+    }
+})
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
