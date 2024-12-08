329f43a56add35f092db3ed9c6cbf717a635215d
Dinesh Magar <magard1010@gmail.com>
2024-12-08 23:59:33 +0545

    static file setup in expjs

---
+++ staticfileinexp/controllers/homeController.js
@@ -0,0 +1,9 @@
+import { join } from 'path'
+
+const homeController = (req, res) => {
+
+    console.log(join(process.cwd(), 'views', 'index.html'))
+    res.sendFile(join(process.cwd(), 'views', 'index.html'))
+}
+
+export { homeController }
