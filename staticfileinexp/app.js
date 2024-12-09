60de1cce866d727a6ac6b767ea1eb35d7c59d73b
Dinesh Magar <magard1010@gmail.com>
2024-12-09 23:59:29 +0545

    img js file use in expjs

--- staticfileinexp/app.js
+++ staticfileinexp/app.js
@@ -1,12 +1,14 @@
 // routing (common for all application)
 
 import express from 'express'
+import { join } from 'path'
 
 const app = express()
 
 const port = process.env.PORT || '3000'
 
 import web from './routes/web.js'
+
 
 // static file use
 app.use(express.static('public'))
