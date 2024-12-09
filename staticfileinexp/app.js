1bf5aa077f9203120dc1762510dc2d19214d8d8c
Dinesh Magar <magard1010@gmail.com>
2024-12-09 23:53:14 +0545

    static file use in expjs

--- staticfileinexp/app.js
+++ staticfileinexp/app.js
@@ -6,6 +6,14 @@ const app = express()
 
 const port = process.env.PORT || '3000'
 
+import web from './routes/web.js'
+
+// static file use
+app.use(express.static('public'))
+
+//load routes
+app.use('/', web)
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
--- staticfileinexp/routes/web.js
+++ staticfileinexp/routes/web.js
@@ -2,8 +2,8 @@ import express from 'express'
 
 const router = express.Router()
 
-import { homeController } from '../controllers/homeController'
+import { homeController } from '../controllers/homeController.js'
 
-router.get('/', homeController)
+router.get("/", homeController)
 
 export default router
