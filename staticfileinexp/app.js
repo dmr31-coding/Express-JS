a587d0afbe3a6dab19974d3b447b9f983b60cbf0
Dinesh Magar <magard1010@gmail.com>
2024-12-10 23:52:42 +0545

    dotfile special directory favicon in expjs

--- staticfileinexp/app.js
+++ staticfileinexp/app.js
@@ -11,10 +11,23 @@ import web from './routes/web.js'
 
 
 // static file use
-app.use(express.static('public'))
+app.use(express.static(join(process.cwd(), 'public')))
+// console.log(express.static(join(process.cwd(), 'public')))
+
+//serve only specific directory then make virtual path
+app.use('/css', express.static(join(process.cwd(), 'public/css')))
+
 
 //load routes
 app.use('/', web)
+
+// dot file use
+const options = {
+    dotfiles: "ignore",
+}
+
+app.use(express.static(join(process.cwd(), 'public'), options))
+
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
