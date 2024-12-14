2ec14405afd00995ba978f78691e4902a88d7ca9
Dinesh Magar <magard1010@gmail.com>
2024-12-14 23:36:51 +0545

    ejs setup start

--- ejsdetail/app.js
+++ ejsdetail/app.js
@@ -6,6 +6,15 @@ const app = express()
 
 const port = process.env.PORT || '3000'
 
+import web from './routes/web.js'
+
+// set template engine
+app.set('view engine', 'ejs')
+
+//load routes
+app.use('/', web)
+
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
