32bad7fe1232fab06705f0cd94bd1969793450c7
Dinesh Magar <magard1010@gmail.com>
2024-12-21 22:07:06 +0545

    application level middleware

--- applevelmiddleware/app.js
+++ applevelmiddleware/app.js
@@ -7,6 +7,14 @@ const app = express()
 
 const port = process.env.PORT || '3000'
 
+import web from './routes/web.js'
+
+// set templatte engine
+app.set('view engine', 'ejs')
+
+// load routes
+app.use('/', web)
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
