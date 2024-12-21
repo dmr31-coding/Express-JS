7d478f0e5e603ddeb9ec9a8452dd83369cd5e830
Dinesh Magar <magard1010@gmail.com>
2024-12-21 21:51:01 +0545

    application level middleware setup

---
+++ applevelmiddleware/app.js
@@ -0,0 +1,13 @@
+
+// routing (common for all application)
+
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || '3000'
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
+
