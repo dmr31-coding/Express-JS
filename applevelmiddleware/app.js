de55d136e5ea98e156ab3c8a19e3553ee677ad10
Dinesh Magar <magard1010@gmail.com>
2024-12-23 16:57:19 +0545

    creating app level middleware

--- applevelmiddleware/app.js
+++ applevelmiddleware/app.js
@@ -8,9 +8,18 @@ const app = express()
 const port = process.env.PORT || '3000'
 
 import web from './routes/web.js'
+import myLogger from './middlewares/logger-middleware.js'
 
 // set templatte engine
 app.set('view engine', 'ejs')
+
+// appliation level middleware
+// app.use(myLogger)
+app.use('/about', myLogger)
+app.use('/about/23', myLogger)
+app.use('/about/34', myLogger)
+
+
 
 // load routes
 app.use('/', web)
