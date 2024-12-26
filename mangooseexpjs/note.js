603c72fe540043cbdc8a14ffba3fd5d22bb716af
Dinesh Magar <magard1010@gmail.com>
2024-12-26 23:58:13 +0545

    mangodb setup  node exp

---
+++ mangooseexpjs/note.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>mangoose</title>
+</head>
+<body>
+    <h1>mangoose chai  package ho jasle mangodb chalauna fast help hunxa</h1>
+    <h2>tesko lagi node and mangodb install hunuparxa</h2>
+    <h3>setup garda connection object hunxa</h3>
+    <h4>without authen. and with authen. gari dui tarika bata garna milxxa</h4>
+    <h5>end</h5>
+</body>
+</html>
